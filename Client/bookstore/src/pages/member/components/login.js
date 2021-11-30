d1a8c518726418b02e14eab38af8f354dc100617
蔡禎宸 <t108590046@ntut.org.tw>
2021-11-30 19:21:54 +0800

    登入功能

--- Client/bookstore/src/pages/member/components/login.js
+++ Client/bookstore/src/pages/member/components/login.js
@@ -1,14 +1,38 @@
-import {Link} from 'react-router-dom';
+import {Link} from 'react-router-dom'
+import { useState } from 'react'
+import axios from 'axios'
 const Login = () => {
+    const [id,setID] = useState('')
+    const [password,setPassword] = useState('')
+    const submit = () => {
+        axios({
+            method: 'POST',
+            url: 'http://localhost:5000/register/',
+            data:{
+              userName: id,
+              userPassword: password,
+            }
+          }).then((response) => {
+            console.log(response.data);
+            if(response.data.state === '200'){
+                window.sessionStorage.setItem('name',response.data.name)
+                window.sessionStorage.setItem('token',response.data.token)
+                alert('登入成功')
+            }
+            else if(response.data.state === '500'){
+                alert('登入失敗')
+              }
+          })
+    }
     return <div>
         <h1>會員登入</h1>
-        <input type='text' name='ID' placeholder='請輸入帳號'></input>
+        <input type='text' name='ID' placeholder='請輸入帳號' value={id} onChange={(e) => {setID(e.target.value)}}></input>
         <br/>
         <br/>
-        <input type='password' name='Password' placeholder='請輸入密碼'></input>
+        <input type='password' name='Password' placeholder='請輸入密碼' value={password} onChange={(e) => {setPassword(e.target.value)}}></input>
         <br/>
         <br/>
-        <button>登入</button>
+        <button onClick={submit}>登入</button>
         <nav>
             <li><Link to="/register">註冊</Link></li>
         </nav>
