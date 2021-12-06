f50b10ccf121fa292e0e0c76a9f72e5a92517f2a
蔡禎宸 <t108590046@ntut.org.tw>
2021-12-07 01:26:55 +0800

    查詢更改個人資料 完成

--- Client/bookstore/src/pages/member/components/accountInfo.js
+++ Client/bookstore/src/pages/member/components/accountInfo.js
@@ -23,7 +23,7 @@ const AccountInfo =()=>{
                   token: window.sessionStorage.getItem('token'),
                 }
               }).then((response) => {
-                if(response.data.state === '200'){
+                  if(response.data.state === 200){
                     setuserName(response.data.userName)
                     setPassword(response.data.userPassword)
                     setName(response.data.name)
@@ -34,13 +34,43 @@ const AccountInfo =()=>{
                     setPaymentInfo(response.data.paymentInfo)
                     setDescription(response.data.description)
                     setLogo(response.data.logo)
-                }
-                else if(response.data.state === '500'){
-                    alert(response.data.error)
                   }
+                  else{
+                      alert('error')
+                  }
+                   
               })
         }
     },[])
+
+    const send=()=>{
+        axios({
+            method: 'POST',
+            url: 'http://localhost:5000/account/update',
+            data:
+            {
+                userName: window.sessionStorage.getItem('userName'),
+                token: window.sessionStorage.getItem('token'),
+                userPassword: password,
+                name:name,
+                gender:gender,
+                email:email,
+                phone:phone,
+                address:address,
+                paymentInfo:paymentInfo,
+                description:description,
+                logo:logo
+            }
+          }).then((response) => {
+              if(response.data.state === 200){
+                alert('更改成功')
+              }
+              else{
+                  alert('error')
+              }
+               
+          })
+    }
 
     if (window.sessionStorage.getItem('token')!== null) {
         return(
@@ -52,14 +82,91 @@ const AccountInfo =()=>{
             <tbody>
                 <tr>
                     <th>
-                        <label>會員帳號</label>
+                        <label>會員帳號:</label>
                     </th>
                     <td>
-                        <input type='text' name="Id" value={userName} onChange={(e) => {setuserName(e.target.value)}} ></input>
+                        <div>{userName}</div>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <label>會員密碼:</label>
+                    </th>
+                    <td>
+                    <input type='text' value={password} onChange={(e) => {setPassword(e.target.value)}}></input>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <label>會員名稱:</label>
+                    </th>
+                    <td>
+                    <input type='text' value={name} onChange={(e) => {setName(e.target.value)}}></input>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <label>會員性別:</label>
+                    </th>
+                    <td>
+                        <input type='radio' name="Gender" value='1' onClick={(e) => {setGender(e.target.value)}}  checked={gender === "1"}></input>
+                        <label>男</label>
+                        <input type='radio' name="Gender" value='0' onClick={(e) => {setGender(e.target.value)}} checked={gender === "0"}></input>
+                        <label>女</label>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <label>E-mail:</label>
+                    </th>
+                    <td>
+                    <input type='text' value={email} onChange={(e) => {setEmail(e.target.value)}}></input>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <label>手機:</label>
+                    </th>
+                    <td>
+                    <input type='text' value={phone} onChange={(e) => {setPhone(e.target.value)}}></input>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <label>地址:</label>
+                    </th>
+                    <td>
+                    <input type='text' value={address} onChange={(e) => {setAddress(e.target.value)}}></input>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <label>paymentInfo:</label>
+                    </th>
+                    <td>
+                    <input type='text' value={paymentInfo} onChange={(e) => {setPaymentInfo(e.target.value)}}></input>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <label>description:</label>
+                    </th>
+                    <td>
+                    <input type='text' value={description} onChange={(e) => {setDescription(e.target.value)}}></input>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <label>logo:</label>
+                    </th>
+                    <td>
+                    <input type='text' value={logo} onChange={(e) => {setLogo(e.target.value)}}></input>
                     </td>
                 </tr>
             </tbody>
+            <button onClick={send}>更改資料</button>
         </table>
+        
         )
     }
     else{
