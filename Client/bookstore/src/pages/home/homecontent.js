7717a2364bc3721b358f0c6c8a262ef73ccfe3fe
t108590017 <t108590017@ntut.org.tw>
2021-12-14 00:57:48 +0800

    首頁Carousels

--- Client/bookstore/src/pages/home/homecontent.js
+++ Client/bookstore/src/pages/home/homecontent.js
@@ -1,4 +1,51 @@
+import { Carousel,Container,Row,Col} from 'react-bootstrap'; 
+import 'bootstrap/dist/css/bootstrap.min.css';        
+import carouselImg1 from './../../image/getImage.jpg';
+import carouselImg2 from './../../image/getImage2.jpg';
+import carouselImg3 from './../../image/getImage3.jpg';
+import carouselImg4 from './../../image/getImage4.jpg';
+import './home.css';
+
 const HomeContent = () =>{
-    return (<div>我是內容</div>)
+    return (
+    <Container className='home'> 
+        <Row>
+                <h1>你好</h1>
+        </Row>
+        <Row>        
+
+            <Col>
+                <Carousel>
+                        <Carousel.Item>
+                            <img
+                            src={carouselImg1}
+                            alt="First slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                            src={carouselImg2}
+                            alt="Second slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                            width={750}
+                            height={240}
+                            src={carouselImg3}
+                            alt="Third slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                            src={carouselImg4}
+                            alt="fourth slide"
+                            />
+                        </Carousel.Item>
+                    </Carousel>
+                </Col>
+        </Row>   
+    </Container>
+    )
 }
 export default HomeContent
