4e0e40a7a220fb626cacc49219e0141c1d69cb65
LI-YONG-QI <a2369028@gmail.com>
2021-12-15 23:13:45 +0800

    'gui'

--- Server/test/test.js
+++ Server/test/test.js
@@ -1,275 +1,119 @@
+const puppeteer = require('puppeteer');
 let {assert, should} = require('chai');
-const express = require('express')
-const database = require('../database/index')
-const datatype = require('../function/datatype')
-const axios = require('axios')
+const express = require('express');
+const database = require('../database/index');
+const datatype = require('../function/datatype');
+const axios = require('axios');
 
-//初次創建測試 建立c1 c2 b1的帳戶
-describe('Account test', function(){ 
-    it("Check whether first customer registration is success in normal", async function(){
-        let res = await axios.post('http://localhost:5000/account/register/', {
-            "userName":"c1",
-            "userPassword": "c1",
-            "name":"customer1",
-            "gender":"1",
-            "email":"customer1@gmail.com",
-            "phone":"091234567",
-            "address":"台南市安平區87號",
-            "type":"customer"
-        })
-        assert.equal(res.data.state, 200);
-        assert.equal(res.data.error, "");
-    })
-    it("Creat whether second customer registration is success if Password is repeated", async function(){
-        let res = await axios.post('http://localhost:5000/account/register/', {
-            "userName":"c2",
-            "userPassword": "c1",
-            "name":"customer2",
-            "gender":"1",
-            "email":"customer2@gmail.com",
-            "phone":"079779797",
-            "address":"北科宿舍冤魂",
-            "type":"customer"
-        })
-        assert.equal(res.data.state, 200);
-        assert.equal(res.data.error, "");
-    })
 
-    it("Creat whether first business registration is success in normal", async function(){
-        let res = await axios.post('http://localhost:5000/account/register/', {
-            "userName":"b1",
-            "userPassword": "b1",
-            "name":"business1",
-            "gender":"1",
-            "email":"business1@gmail.com",
-            "phone":"012145464",
-            "address":"信義區",
-            "type":"business"
-        })
-        assert.equal(res.data.state, 200);
-        assert.equal(res.data.error, "");
+describe('Register account test', function(){
+    before(async ()=> {
+        msg = ''
+        browser = await puppeteer.launch();
+        page = await browser.newPage();
+        await page.setViewport({ width: 1920, height: 1080});
+        await page.goto('http://localhost:3000/');
+        
+
+        page.on('dialog', async dialog => {
+            console.log(dialog.message());
+            await dialog.accept();
+        });
+
+        userName = '#root > div.register > table > tbody > tr:nth-child(1) > td > input[type=text]'
+        Name = '#root > div.register > table > tbody > tr:nth-child(2) > td > input[type=text]'
+        Email = '#root > div.register > table > tbody > tr:nth-child(3) > td > input[type=text]'
+        Password = '#root > div.register > table > tbody > tr:nth-child(4) > td > input[type=password]'
+        radioMan = '#root > div.register > table > tbody > tr:nth-child(5) > td > input[type=radio]:nth-child(1)'
+        Phone = '#root > div.register > table > tbody > tr:nth-child(6) > td > input[type=tel]'
+        Address = '#root > div.register > table > tbody > tr:nth-child(7) > td > input[type=text]'
+        Type = '#root > div.register > table > tbody > tr:nth-child(8) > td > select'
+        Register = '#root > div.register > button'
+
+        login_userName = '#root > div.login > div:nth-child(2) > p > input[type=text]'
+        login_Password = '#root > div.login > div:nth-child(3) > p > input[type=password]'
+        Login = '#root > div.login > div:nth-child(4) > button'
+
+        Logout = '#navbarScroll > div > a:nth-child(2)'
+        Navbar = '#root > div:nth-child(1) > nav'
+    });
+
+    after(async ()=>{
+        await browser.close();
     })
 
-    it("Creat whether first business registration is success if Password is repeated", async function(){
-        let res = await axios.post('http://localhost:5000/account/register/', {
-            "userName":"b2",
-            "userPassword": "b1",
-            "name":"business2",
-            "gender":"1",
-            "email":"business2@gmail.com",
-            "phone":"045444444",
-            "address":"中山區",
-            "type":"business"
-        })
-        assert.equal(res.data.state, 200);
-        assert.equal(res.data.error, "");
-    })
-})
+    it("Check whether first customer c1 registration is success in normal", async () => {
+        await database.sqlConnection('delete from `account`');
+        await database.sqlConnection('delete from `customer`');
+        await database.sqlConnection('delete from `business`');
+        
+        await page.click('#navbarScroll > div > a:nth-child(1)') //點選Login
+        await page.waitForSelector('#root > div.login > div:nth-child(4) > nav > li > a') //等待頁面跳轉
+        await page.click('#root > div.login > div:nth-child(4) > nav > li > a') // 進入註冊畫面
 
-//Customer創建失敗測試 嘗試建立c3的帳戶
-describe('Customer test Failed', function(){ 
-    it("Check whether registration is failed if userName is repeated", async () =>{
-        let res = await axios.post('http://localhost:5000/account/register/', {
-            "userName":"c1",
-            "userPassword": "c3",
-            "name":"customer3",
-            "gender":"1",
-            "email":"customer3@gmail.com",
-            "phone":"091213546",
-            "address":"嘉義市自強街",
-            "type":"customer"
-        })
-        assert.equal(res.data.state, 500);
-        assert.equal(res.data.error, "註冊失敗");
+        await page.waitForSelector(userName);
+        await page.type(userName, 'c1'); 
+        await page.type(Name, 'customer1');
+        await page.type(Email, 'customer1@gmail.com');
+        await page.type(Password, 'c1');
+        await page.click(radioMan); 
+        await page.type(Phone, '0912345671');
+        await page.type(Address, '嘉義市');
+        await page.click(Register);
+        await page.screenshot({path: "fuck.png"})
+        
+
+        await page.waitForTimeout(1000); //稍微等待後再關閉
+
+        //assert.equal(msg, '註冊成功')
     })
 
+    /*it('Check whether first customer c1 login is success in normal', async () => {
+        await page.click('#navbarScroll > div > a:nth-child(1)') //點選Login 
+        await page.waitForSelector(login_userName);
+        await page.type(login_userName, 'c1');      
+        await page.type(login_Password, 'c1');       
+        await page.click(Login);
 
-    it("Check whether registration is failed if email is repeated", async () =>{
-        let res = await axios.post('http://localhost:5000/account/register/', {
-            "userName":"c3",
-            "userPassword": "c3",
-            "name":"customer3",
-            "gender":"1",
-            "email":"customer1@gmail.com",
-            "phone":"091213546",
-            "address":"嘉義市自強街",
-            "type":"customer"
-        })
-        assert.equal(res.data.state, 500);
-        assert.equal(res.data.error, "註冊失敗");
-    })
+        await page.waitForSelector('#navbarScroll > div > a.nav-link.disabled');//稍微等待後再關閉
 
-
-    /*it("Check whether registration is failed if phone is empty", async () =>{
-        let res = await axios.post('http://localhost:5000/account/register/', {
-            "userName":"c3",
-            "userPassword": "c3",
-            "name":"customer3",
-            "gender":"1",
-            "email":"customer3@gmail.com",
-            "phone":"",
-            "address":"嘉義市自強街",
-            "type":"customer"
-        })
-        assert.equal(res.data.state, 500);
-        assert.equal(res.data.error, "註冊失敗");
+        assert.equal(msg, '登入成功')
     })*/
 
-    it("Check whether registration is failed if phone is repeated", async () =>{
-        let res = await axios.post('http://localhost:5000/account/register/', {
-            "userName":"c3",
-            "userPassword": "c3",
-            "name":"customer3",
-            "gender":"1",
-            "email":"customer3@gmail.com",
-            "phone":"091234567",
-            "address":"嘉義市自強街",
-            "type":"customer"
-        })
-        assert.equal(res.data.state, 500);
-        assert.equal(res.data.error, "註冊失敗");
-    })
+    /*c3
+    "userName":"c3",
+    "userPassword": "c3",
+    "name":"customer3",
+    "gender":"1",
+    "email":"customer3@gmail.com",
+    "phone":"0912135461",
+    "address":"嘉義市自強街",
+    "type":"customer"
+    */
+    /*it("Check whether first customer c3 registration is success if userName is repeated", async () => {   
+        console.log(msg)
+        
+        await page.click(Logout) //點選Login
+        await page.waitForSelector('#root > div.login > div:nth-child(4) > nav > li > a') //等待頁面跳轉        
+        r = '#root > div.login > div:nth-child(4) > nav > li > a'
+        await page.click(r) // 進入註冊畫面
+        
+        
+        
+        await page.waitForSelector(userName);
+        await page.type(userName, 'c1'); 
+        await page.type(Name, 'customer3');
+        await page.type(Email, 'customer3@gmail.com');
+        await page.type(Password, 'c3');
+        await page.click(radioMan); 
+        await page.type(Phone, '0912135461');
+        await page.type(Address, '中山區');
+        await page.click(Register);
+        
+        await page.screenshot({path: 'fuck123456.png'})
+        await page.waitForSelector('#root > div:nth-child(1) > nav'); //稍微等待後再關閉
 
+        assert.equal(msg, '註冊失敗')
+    })*/
+});
 
-    it("Check whether registration is failed if type is empty", async () =>{
-        let res = await axios.post('http://localhost:5000/account/register/', {
-            "userName":"c3",
-            "userPassword": "c3",
-            "name":"customer3",
-            "gender":"1",
-            "email":"customer3@gmail.com",
-            "phone":"091213546",
-            "address":"嘉義市自強街",
-            "type":""
-        })
-        assert.equal(res.data.state, 500);
-        assert.equal(res.data.error, "未指定用戶類別");
-    })
-})
-
-//Business失敗測試 嘗試建立b3的帳戶
-describe('Business test Failed', function(){ 
-    it("Check whether registration is failed if userName is repeated", async () =>{
-        let res = await axios.post('http://localhost:5000/account/register/', {
-            "userName":"b1",
-            "userPassword": "b3",
-            "name":"business3",
-            "gender":"1",
-            "email":"business3@gmail.com",
-            "phone":"097533253",
-            "address":"中國西山省",
-            "type":"business"
-        })
-        assert.equal(res.data.state, 500);
-        assert.equal(res.data.error, "註冊失敗");
-    })
-
-
-    it("Check whether registration is failed if email is repeated", async () =>{
-        let res = await axios.post('http://localhost:5000/account/register/', {
-            "userName":"b3",
-            "userPassword": "b1",
-            "name":"business3",
-            "gender":"1",
-            "email":"business1@gmail.com",
-            "phone":"097533253",
-            "address":"中國西山省",
-            "type":"business"
-        })
-        assert.equal(res.data.state, 500);
-        assert.equal(res.data.error, "註冊失敗");
-    })
-
-    it("Check whether registration is failed if phone is repeated", async () =>{
-        let res = await axios.post('http://localhost:5000/account/register/', {
-            "userName":"b3",
-            "userPassword": "b3",
-            "name":"business3",
-            "gender":"1",
-            "email":"business1@gmail.com",
-            "phone":"012145464",
-            "address":"中國西山省",
-            "type":"business"
-        })
-        assert.equal(res.data.state, 500);
-        assert.equal(res.data.error, "註冊失敗");
-    })
-
-    it("Check whether registration is failed if type is empty", async () =>{
-        let res = await axios.post('http://localhost:5000/account/register/', {
-            "userName":"b3",
-            "userPassword": "b1",
-            "name":"business3",
-            "gender":"1",
-            "email":"business1@gmail.com",
-            "phone":"097533253",
-            "address":"中國西山省",
-            "type":""
-        })
-        assert.equal(res.data.state, 500);
-        assert.equal(res.data.error, "未指定用戶類別");
-    })
-})
-
-
-//登入測試
-describe('Login test', function(){ 
-    it("Check whether customer login is success in normal", async () =>{
-        let res = await axios.post('http://localhost:5000/account/login/', {
-            "userName": "c1",
-            "userPassword": "c1",
-        })
-        assert.equal(res.data.state, 200);
-        assert.equal(res.data.type, "customer");
-        assert.equal(res.data.name, "customer1");
-        assert.notEqual(res.data.token, "");
-    })
-
-    it("Check whether customer login is failed if userPassword is not correct", async () =>{
-        let res = await axios.post('http://localhost:5000/account/login/', {
-            "userName": "c1",
-            "userPassword": "c3",
-        })
-        assert.equal(res.data.state, 500);
-        assert.equal(res.data.error, "登入失敗");
-    })
-
-    //目前因為會插入資料庫 所以會顯示登入成功
-    it("Check whether customer login is failed if userName and userPassword are not exist", async () =>{
-        let res = await axios.post('http://localhost:5000/account/login/', {
-            "userName": "c3",
-            "userPassword": "c3",
-        })
-        assert.equal(res.data.state, 500);
-        assert.equal(res.data.error, "登入失敗");
-    })
-
-    it("Check whether business login is success in normal ", async () =>{
-        let res = await axios.post('http://localhost:5000/account/login/', {
-            "userName": "b1",
-            "userPassword": "b1",
-        })
-        assert.equal(res.data.state, 200);
-        assert.equal(res.data.type, "business");
-        assert.equal(res.data.name, "business1");
-        assert.notEqual(res.data.token, "");
-    })
-
-    it("Check whether business login is failed if userPassword is not correct", async () =>{
-        let res = await axios.post('http://localhost:5000/account/login/', {
-            "userName": "b1",
-            "userPassword": "b3",
-        })
-        assert.equal(res.data.state, 500);
-        assert.equal(res.data.error, "登入失敗");
-    })
-
-    it("Check whether business login is failed if userName and userPassword are not exist", async () =>{
-        let res = await axios.post('http://localhost:5000/account/login/', {
-            "userName": "b3",
-            "userPassword": "b3",
-        })
-        assert.equal(res.data.state, 500);
-        assert.equal(res.data.error, "登入失敗");
-    })
-})
