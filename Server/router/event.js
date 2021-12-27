b9bfff34df469f161cd8fe9cf94e1954554fbe06
DandinPower <t108590045@ntut.org.tw>
2021-12-28 01:30:17 +0800

    新增優惠券api

--- Server/router/event.js
+++ Server/router/event.js
@@ -435,4 +435,169 @@ router.post('/delete', datatype.verifyToken, async(req,res,next)=>{
     }
     
 })
+
+router.post('/coupon/add', datatype.verifyToken, async(req,res,next)=>{
+    var type = req.body.type 
+    var userName = req.body.userName
+    var name = req.body.name
+    var code = req.body.code
+    var date = req.body.date 
+    var _date = new Date(date) 
+    var currentDate = Date.now()
+    var discount = req.body.discount 
+    var maxQuantity = req.body.maxQuantity
+    if (type != 'business' & type != 'admin'){
+        let response = {
+            "error":"不是business或admin",
+            "state":500
+        }
+        res.json(response)
+    }
+    if (code.length >= 10 | code.length <= 0){
+        let response = {
+            "error":"優惠碼不符合限制",
+            "state":500
+        }
+        res.json(response)
+    }
+    if (_date < currentDate){
+        let response = {
+            "error":"到期日不符合限制",
+            "state":500
+        }
+        res.json(response)
+    }
+    if (discount <= 0 | discount >1){
+        let response = {
+            "error":"折扣不符合限制",
+            "state":500
+        }
+        res.json(response)
+    }
+    if (maxQuantity <= 0){
+        let response = {
+            "error":"最大數量不符合限制",
+            "state":500
+        }
+        res.json(response)
+    }
+    if (type == 'business'){
+        try{
+            var userId = await database.GetUserId(userName)
+            if (userId == null){
+                let response = {
+                    "error":"找不到該用戶",
+                    "state":500
+                }
+                res.json(response)
+            }
+        }catch(e){
+            console.log(e)
+            let response = {
+                "error":"網路連線失敗",
+                "state":500
+            }
+            res.json(response)
+        }
+    }
+    else if (type == 'admin'){
+        try{
+            var userId = await database.GetAdminId(userName)
+            if (userId != null){
+                var adminResult = await database.sqlConnection(`select authority from admin where id = ${userId}`)
+                if (adminResult[0].authority != 'all' & adminResult[0].authority != 'event'){
+                    let response = {
+                        "error":"管理員沒有該權限",
+                        "state":500
+                    }
+                    res.json(response)
+                }
+            }
+            else{
+                let response = {
+                    "error":"找不到該用戶",
+                    "state":500
+                }
+                res.json(response)
+            }
+            
+        }catch(e){
+            let response = {
+                "error":"網路連線失敗",
+                "state":500
+            }
+            res.json(response)
+        }  
+    }
+    try{
+        var organizerId = await database.GetOrganizerId(type,userId)
+        console.log(organizerId)
+        if (organizerId != null){
+            try{
+                var nameResult = await database.sqlConnection(`select name from event where organizerId = ${organizerId};`)
+                nameResult = datatype.packet2list(nameResult,"name")
+                console.log(nameResult)      
+                if (nameResult.includes(name)){
+                    const sqlInsert = `insert into coupon(code,eventName,date,discount,maxQuantity)value("${code}","${name}","${date}",${discount},${maxQuantity});`       
+                    console.log(sqlInsert)
+                    try{
+                        var result = await database.sqlConnection(sqlInsert)
+                        console.log(result)
+                        let response = {
+                            "error":"",
+                            "state":200
+                        }
+                        res.json(response)
+                    }catch(e){
+                        console.log(e)
+                        let response = {
+                            "error":"活動名不符合限制",
+                            "state":500
+                        }
+                        res.json(response)
+                    }
+                }
+                else{
+                    let response = {
+                        "error":"找不到該活動",
+                        "state":500
+                    }
+                    res.json(response)
+                }
+            }catch(e){
+                console.log(e)
+                let response = {
+                    "error":"網路連線失敗",
+                    "state":500
+                }
+                res.json(response)
+            }
+        }
+        else{
+            let response = {
+                "error":"找不到該活動",
+                "state":500
+            }
+            res.json(response)
+        }
+    }catch(e){
+        let response = {
+            "error":"網路連線失敗",
+            "state":500
+        }
+        res.json(response)
+    }
+})
+
+router.post('/coupon/search', datatype.verifyToken, async(req,res,next)=>{
+
+})
+
+router.post('/coupon/update', datatype.verifyToken, async(req,res,next)=>{
+
+})
+
+router.post('/coupon/delete', datatype.verifyToken, async(req,res,next)=>{
+
+})
 module.exports = router
