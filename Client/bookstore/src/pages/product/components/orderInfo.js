113368c6149e465fd8d53a2019181131f589f505
LI-YONG-QI <a2369028@gmail.com>
2022-01-05 20:22:40 +0800

    data.image add

--- Client/bookstore/src/pages/product/components/orderInfo.js
+++ Client/bookstore/src/pages/product/components/orderInfo.js
@@ -257,6 +257,7 @@ const OrderInfo = ({checkOrderInfo})=>{
             }
             return(
               <Row>
+                <Col><img src={`data:image/png;base64,${data.image}`} width="200" height="200"></img></Col>
                 <Col className="w-20"></Col>
                 <Col className="w-20">{data.name}</Col>
                 <Col className="w-20"><input type='number' value={data.quantity} onChange={e => changeQuantity(data.productId,e.target.value)}></input></Col>
@@ -299,11 +300,8 @@ const OrderInfo = ({checkOrderInfo})=>{
         </div>
         <br size="lg"/>
         <div className="d-flex justify-content-end">
-          <h1 className="me-4">總金額:</h1>
+          <h1 className="me-4">總金額:{price}</h1>
           <Button variant="success" size="lg" onClick={ e => OrderBooks()}>下訂</Button>
-        </div>
-        <div className="d-flex justify-content-end">        
-          
         </div>
       </Container>
     </Container>)
